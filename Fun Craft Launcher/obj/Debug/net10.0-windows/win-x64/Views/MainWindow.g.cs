ef30ab3399eba3807cc14fae7438d6945d5279fb
146wyx <wyx15799144671@outlook.com>
2026-02-22 18:02:55 +0800

    0.2

--- Fun Craft Launcher/obj/Debug/net10.0-windows/win-x64/Views/MainWindow.g.cs
+++ Fun Craft Launcher/obj/Debug/net10.0-windows/win-x64/Views/MainWindow.g.cs
@@ -220,7 +220,7 @@ namespace FunCraftLauncher.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FCL;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fun Craft Launcher;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
